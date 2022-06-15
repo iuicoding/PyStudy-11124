9b255199495c968ba9e8d4c06a5b0366efad5beb
GiftShower_ <58767497+GiftShower@users.noreply.github.com>
2022-06-15 18:20:31 +0900

    Update code_study.py

--- pystudy_1/code_study.py
+++ pystudy_1/code_study.py
@@ -1,1 +1,18 @@
-print('hello world!!')
+print(
+    """
+    여러 줄 문자열
+    
+    줄이 복사가 된다고!
+    """)
+
+print("여러 줄 \n 줄이 복사가 되네")
+
+print("""의미는 없음\
+그냥 줄 바꿈 입니다.
+""")
+
+print("그것이"+"약속이니까")
+print("끄덕"*3)
+
+string=input("인사말을 입력하세요")
+print(string)
